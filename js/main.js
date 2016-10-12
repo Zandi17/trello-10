173db94a916a985a4a1de5a090f11b801109cb8f
Sandy Ciquero <sandy-ciqueroch@gmail.com>
2016-10-12 10:16:01 -0500

    añadir lista

--- js/main.js
+++ js/main.js
@@ -11,6 +11,48 @@ window.addEventListener("load", function() {
 		lista.classList.add("ocultar")
 		formulario.classList.remove("ocultar");
 	});
+		btnGuardar.addEventListener("click",function(e){
+		e.preventDefault();
+		pubTitulo();
+	});
+
+	function pubTitulo(){
+		formulario.classList.add("ocultar");
+		var	divTitulo = document.createElement("div");
+		var titulo = document.createElement("span");
+		var contenido = input.value;
+		var enlace = document.createElement("a");
+		
+		titulo.textContent = contenido;
+		divTitulo.classList.add("fondo");
+		enlace.textContent = "Añadir una Tarjeta";
+		
+		contenedor.appendChild(divTitulo);
+		divTitulo.appendChild(titulo);
+		contenedor.appendChild(enlace);
+
+		// var inicio = document.createElement("div");
+		// boxPadre.appendChild(inicio);
+		// inicio.classList.add("lado");
+		// inicio.appendChild(lista);
+		// lista.classList.remove("ocultar");
+
+		// enlace.addEventListener("click",function(e){
+		// 	e.preventDefault();
+		// 	nuevaTarjeta(this);
+		// });
+		// 	function nuevaTarjeta(){
+		// 		var formTxtArea = document.createElement("form")
+		// 		var txtArea = document.createElement("textarea");
+		// 		var boton = document.createElement("button");
+
+		// 		boton.textContent = "Guardar";
+
+		// 		divTitulo.appendChild(formTxtArea)
+		// 		formTxtArea.appendChild(txtArea);
+		// 		formTxtArea.appendChild(boton);
+		// 	}
+	}
 
 });
 
